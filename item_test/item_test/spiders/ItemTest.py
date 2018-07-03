b9024d2c19cd2ab634f9be13d0087ba89b5a9881
long5313828 <longzheyi666@foxmail.com>
2018-07-03 15:24:26 +0800

    change

---
+++ item_test/item_test/spiders/ItemTest.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from item_test.items import ItemTestItem
+
+class ItemtestSpider(scrapy.Spider):
+    name = 'ItemTest'
+    allowed_domains = ['cnblogs.com']
+    start_urls = ['http://www.cnblogs.com/qiyeboy/default.html?page=1']
+
+    def parse(self, response):
+        papers = response.xpath(".//*[@class='day']")
+        for paper in papers:
+            url = paper.xpath(".//*[@class='postTitle']/a/@href").extract()[0].encode('utf-8')
+            title = paper.xpath(".//*[@class='postTitle']/a/text()").extract()[0].encode('utf-8')
+            time = paper.xpath(".//*[@class='dayTitle']/a/text()").extract()[0].encode('utf-8')
+            content = paper.xpath("//*[@class='postCon']/div/text()").extract()[0].encode('utf-8')
+            item = ItemTestItem(url=url, title=title, time=time, content=content)
+            yield item
+        pass
